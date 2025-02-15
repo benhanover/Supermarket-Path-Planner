b661864a09ffa6d7960890b6c11847c723dc32a9
Ben <benhanover4@gmail.com>
2025-02-15 09:17:51 +0200

    changed header

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ function App() {
       next: (data) => setTodos([...data.items]),
     });
   }, []);
-
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
@@ -21,10 +20,10 @@ function App() {
     client.models.Todo.delete({ id });
   }
 
-  const { signOut } = useAuthenticator();
+  const { user, signOut } = useAuthenticator();
   return (
     <main>
-      <h1>My todos!!!!</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
