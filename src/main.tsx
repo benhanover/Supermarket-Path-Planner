338c13007ad30782f97850c1b1c08258432a0278
Ben <benhanover4@gmail.com>
2025-02-19 18:53:52 +0200

    ignoring missing file

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,11 @@ import { Authenticator } from "@aws-amplify/ui-react";
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
-Amplify.configure(outputs);
+//@ts-ignore
+import outputs from "../amplify_outputs.json";
 
+Amplify.configure(outputs);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Authenticator.Provider>
