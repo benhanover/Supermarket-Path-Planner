3f51703829f3d2109629c590bbcda078067ad101
Ben <benhanover4@gmail.com>
2025-02-19 19:09:25 +0200

    testing CI - 3

--- src/main.tsx
+++ src/main.tsx
@@ -27,8 +27,10 @@ import "@aws-amplify/ui-react/styles.css";
 // Load amplify_outputs.json dynamically to prevent build failures in CI
 async function configureAmplify() {
   try {
+    console.log("1");
     // @ts-ignore
     const outputs = await import("../amplify_outputs.json");
+    console.log("2");
     Amplify.configure(outputs.default);
   } catch (error) {
     console.warn("amplify_outputs.json not found.");
