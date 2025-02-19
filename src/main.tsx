b121603dc440a7c01afaa593fe4deef785f74a40
Ben <benhanover4@gmail.com>
2025-02-19 19:16:29 +0200

    testing CI - 5

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { Authenticator } from "@aws-amplify/ui-react";
-// import App from "./App.tsx";
-// import "./index.css";
-// import { Amplify } from "aws-amplify";
-// import "@aws-amplify/ui-react/styles.css";
-// import outputs from "../amplify_outputs.json";
-
-// Amplify.configure(outputs);
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-//   <React.StrictMode>
-//     <Authenticator.Provider>
-//       <App />
-//     </Authenticator.Provider>
-//   </React.StrictMode>
-// );
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Authenticator } from "@aws-amplify/ui-react";
@@ -23,22 +5,9 @@ import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
+import outputs from "../amplify_outputs.json";
 
-// Load amplify_outputs.json dynamically to prevent build failures in CI
-async function configureAmplify() {
-  try {
-    console.log("1");
-    // @ts-ignore
-    const outputs = await import("../amplify_outputs.json");
-    console.log("2");
-    Amplify.configure(outputs.default);
-  } catch (error) {
-    console.warn("amplify_outputs.json not found.");
-  }
-}
-
-configureAmplify();
-
+Amplify.configure(outputs);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Authenticator.Provider>
