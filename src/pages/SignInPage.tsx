8604b59d6fc68431677a137786ae1d1bada564fb
Ben <benhanover4@gmail.com>
2025-04-30 12:34:35 +0300

    fixed typescript unused

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import { useAppContext } from "../context/AppContext";
